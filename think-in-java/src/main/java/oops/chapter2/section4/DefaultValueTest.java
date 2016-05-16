cedda0934ab07222e3546831a5ff7e31fbf21d60
Lil Zhang <lilzhang90s@163.com>
2016-05-16 22:50:32 +0800

    201605162250

---
+++ think-in-java/src/main/java/oops/chapter2/section4/DefaultValueTest.java
@@ -0,0 +1,46 @@
+/*
+ *
+ * Copyright (c) 2010-2015 by Shanghai HanTao Information Co., Ltd.
+ * All rights reserved.
+ *
+ */
+
+package oops.chapter2.section4;
+
+/**
+ * Description:
+ * <p/>
+ * Create Author  : lilzhang
+ * Create Date    : 2016-05-16
+ * Project        : desultory-essay
+ * File Name      : DefaultValueTest.java
+ */
+public class DefaultValueTest
+{
+    public static void main(String[] args)
+    {
+        Obj obj = new Obj();
+
+        //all true
+        System.out.println(obj.b == false);
+        System.out.println(obj.c == '\u0000');
+        System.out.println(obj.by == (byte) 0);
+        System.out.println(obj.s == (short) 0);
+        System.out.println(obj.i == 0);
+        System.out.println(obj.l == 0L);
+        System.out.println(obj.f == 0f);
+        System.out.println(obj.d == 0d);
+    }
+
+    public static class Obj
+    {
+        boolean b;
+        char c;
+        byte by;
+        short s;
+        int i;
+        long l;
+        float f;
+        double d;
+    }
+}
