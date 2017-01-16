157ac24e9655063ceb39233e96f588c0013f05ac
lilzhang <lilzhang90s@163.com>
2017-01-16 16:03:03 +0800

    20170116

--- serlvet-jsp-a-tutorial/src/main/java/oops/App.java
+++ serlvet-jsp-a-tutorial/src/main/java/oops/App.java
@@ -1,13 +1,60 @@
 package oops;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final String FILE_PATH = "/your/file/path/here.avi";
+
     public static void main( String[] args )
     {
-        System.out.println("hello world");
+        InputStream in = null;
+        try
+        {
+            in = new BufferedInputStream(new FileInputStream(FILE_PATH));
+            int i;
+            while ((i = (in.read())) != -1)
+            {
+                char c = ((char) i);
+                char decoded = decode(c);
+                if (decoded == '/')
+                {
+                    // do something
+                }
+            }
+
+            System.out.println();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (in != null)
+            {
+                try
+                {
+                    in.close();
+                }
+                catch (IOException e)
+                {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    private static char decode(char c)
+    {
+        // your decode rule here
+        return c;
     }
 }
