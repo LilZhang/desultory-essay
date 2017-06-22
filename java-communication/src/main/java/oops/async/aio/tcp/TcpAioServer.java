fbc4b54116fa2ded25f0bb53091d5acb8427337f
lilzhang90s <lilzhang90s@163.com>
2017-06-22 18:33:06 +0800

    201706221833

--- java-communication/src/main/java/oops/async/aio/tcp/TcpAioServer.java
+++ java-communication/src/main/java/oops/async/aio/tcp/TcpAioServer.java
@@ -2,32 +2,125 @@ package oops.async.aio.tcp;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TcpAioServer
 {
-    public static void main(String[] args) throws IOException
+    private final int port;
+
+    private volatile boolean started = false;
+
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public TcpAioServer(int port)
     {
-        final AsynchronousServerSocketChannel listener =
-                AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(5000));
+        this.port = port;
+//        startup();
+    }
 
-        listener.accept(null, new CompletionHandler<AsynchronousSocketChannel,Void>()
+    public void startup()
+    {
+        if (started)
         {
-            public void completed(AsynchronousSocketChannel ch, Void att)
+            return;
+        }
+
+        new Thread(() ->
+        {
+            try
             {
-                // accept the next connection
-                listener.accept(null, this);
+                AsynchronousChannelGroup threadGroup = AsynchronousChannelGroup.withCachedThreadPool(executorService, 1);
+                try (AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(threadGroup))
+                {
+                    server.bind(new InetSocketAddress(port));
 
-                // handle this connection
-//                handle(ch);
+                    System.out.printf("TcpAioServer now listen on %d\n", port);
+
+                    server.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>()
+                    {
+                        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+
+                        @Override
+                        public void completed(AsynchronousSocketChannel result, Object attachment)
+                        {
+                            System.out.println("waiting...");
+
+                            try
+                            {
+                                byteBuffer.clear();
+                                result.read(byteBuffer).get();
+                                // echo
+                                byteBuffer.flip();
+
+                                System.out.println("received: " + new String(byteBuffer.array()));
+                                System.out.println("echo it!");
+
+                                result.write(byteBuffer);
+                                byteBuffer.flip();
+                            }
+                            catch (InterruptedException | ExecutionException e)
+                            {
+                                e.printStackTrace();
+                            }
+                            finally
+                            {
+                                try
+                                {
+                                    result.close();
+                                    server.accept(null, this);
+                                }
+                                catch (Exception e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            System.out.println("done...");
+                        }
+
+                        @Override
+                        public void failed(Throwable exc, Object attachment)
+                        {
+                            exc.printStackTrace();
+                        }
+                    });
+
+                    try
+                    {
+                        // keep running
+                        latch.await();
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
-
-            public void failed(Throwable exc, Void att)
+            catch (IOException e)
             {
-
+                e.printStackTrace();
             }
-        });
+        }, "AIO_AIO").start();
+    }
+
+    public void shutdown()
+    {
+        latch.countDown();
+        started = false;
+    }
+
+    public static void main(String[] args)
+    {
+
     }
 }
